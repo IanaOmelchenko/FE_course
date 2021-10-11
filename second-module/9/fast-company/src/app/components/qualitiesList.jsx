368967bff7796a3acf7d10b3a1253fafaee7d225
Iana <Omelchenko@work.co>
2021-10-10 23:02:50 -0400

    lesson 9

---
+++ second-module/9/fast-company/src/app/components/qualitiesList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Quality from "./quality";
+
+const QualitiesList = ({ qualities }) => {
+    return (
+        <>
+            {qualities && qualities.map((qual) => (
+                <Quality key={qual._id} {...qual} />
+            ))}
+        </>
+    );
+};
+
+QualitiesList.propTypes = {
+    qualities: PropTypes.array
+};
+
+export default QualitiesList;
